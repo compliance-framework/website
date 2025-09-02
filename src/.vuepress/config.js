61392117355ac51a52ee63feab5816a0ff3644f5
ian <ian.miell@container-solutions.com>
2025-09-02 15:40:20 +0100

    Add yandex metrics

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -23,6 +23,35 @@ export default defineUserConfig({
     }
   }),
 
+  head: [
+    [
+      'script',
+      { type: 'text/javascript' },
+      `
+      (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103993750', 'ym');
+
+      ym(103993750, 'init', {
+        ssr:true,
+        webvisor:true,
+        clickmap:true,
+        ecommerce:"dataLayer",
+        accurateTrackBounce:true,
+        trackLinks:true
+      });
+      `
+    ],
+    [
+      'noscript',
+      {},
+      '<div><img src="https://mc.yandex.ru/watch/103993750" style="position:absolute; left:-9999px;" alt="" /></div>'
+    ]
+  ],
+
   theme: defaultTheme({
     sidebar: {
       '/docs/': docsSidebar,
